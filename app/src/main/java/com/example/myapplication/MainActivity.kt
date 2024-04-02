b62e971fb5a05b78bf9f98a28672429fbf2a26fd
judith <judith.ernstberger@rwth-aachen.de>
2024-04-02 22:31:18 +0200

    add three buttons to start view

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,17 +1,33 @@
 package com.example.myapplication
 
+import android.icu.number.NumberFormatter.UnitWidth
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.theme.MyApplicationTheme
-//import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,11 +40,60 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     //call function
-                    Greeting("Android")
+                    //Greeting("Android")
+                    Start()
                 }
             }
         }
     }
+}
+
+@Composable
+fun Start(){
+    var headerText by remember {
+        mutableStateOf("Choose a dish to start!")
+    }
+    var entryText by remember {
+        mutableStateOf("Entry")
+    }
+    var mainText by remember {
+        mutableStateOf("Main")
+    }
+    var dessertText by remember {
+        mutableStateOf("Dessert")
+    }
+
+        Column {
+            Text(text = headerText,
+                modifier = Modifier.padding(16.dp),
+                textAlign = TextAlign.Center)
+            Row {
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier.padding(16.dp),
+                    shape = CircleShape
+                )
+                    {
+                        Text(entryText)
+                    }
+
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier.padding(16.dp),
+                    shape = CircleShape)
+                    {
+                      Text(mainText)
+                    }
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier.padding(16.dp),
+                    shape = CircleShape,
+                    ) {
+                    Text(dessertText)
+                }
+            }
+        }
+
 }
 
 @Composable
@@ -43,6 +108,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     MyApplicationTheme {
-        Greeting("Judith")
+        //Greeting("Judith")
+        Start()
     }
 }
